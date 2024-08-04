ffd76735bfbe59bd06bb891bbbe87a5e00cc742f
Tanya Kostromitskaya <tetian.kos@yandex.ru>
2024-08-04 23:22:16 +0300

    feat: add project

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    sourcemap: true, // enable production source maps
+    rollupOptions: {
+      input: {
+        index: './index.html',
+        menu: './menu.html'
+      }
+    }
+  },
+  css: {
+    devSourcemap: true // enable CSS source maps during development
+  },
+  base: "/tanykos/coffee-house/"
+})
